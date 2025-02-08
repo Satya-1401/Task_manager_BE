ac300ece3407b4218224d4758cd637d68111b863
Satya-1401 <satyasai.kuchibhotla@gmail.com>
2025-02-08 19:19:12 +0530

    Added Task_manager-BE- files

---
+++ src/main/java/com/example/Task_manager_BE/TaskManagerBeApplication.java
@@ -0,0 +1,14 @@
+package com.example.Task_manager_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagerBeApplication.class, args);
+		System.out.println("Hello World...");
+	}
+
+}
